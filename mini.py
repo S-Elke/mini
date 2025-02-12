8a70238bbd12a1821ca24c4761339ea5214ad994
enderpyy <widderender@gmail.com>
2025-02-11 19:32:11 -0500

    Added instructions

---
+++ mini.py
@@ -0,0 +1,9 @@
+'''
+1. Display the list of available courses.
+2. Allow a student to register for courses, with a maximum limit of three courses.
+3. Display the courses a student has registered for.
+4. Allow the student to exit the system.
+Each task will be implemented using separate functions to improve code modularity and
+readability.
+.
+'''
